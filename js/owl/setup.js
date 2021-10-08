44cb88420950aed55d10030937e761f86f62bec4
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-10-08 00:51:01 -0300

    Adicionado footer e alterado a quantidade de itens slides

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -4,13 +4,13 @@ $(".owl-carousel").owlCarousel({
   nav: false,
   responsive: {
     0: {
-      items: 2,
+      items: 1,
     },
     600: {
-      items: 4,
+      items: 3,
     },
     1000: {
-      items: 6,
+      items: 4,
     },
   },
 });
