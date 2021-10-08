9896e6082f7ba24f4cc7640dbd3ba7e57fc9383d
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-10-08 00:34:01 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,58 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: false,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 6,
+    },
+  },
+});
+
+// Selecao do livro ativo na area principal
+// seleciona o livro de acordo com o data-id do livro cover
+function mainBookChange(id) {
+  const mainBook = document.querySelectorAll(".mainBook");
+
+  for (livro of mainBook) {
+    livro.classList.remove("ativo");
+  }
+
+  document.querySelector(`[data-idBook='${id}']`).classList.add("ativo");
+}
+
+// seleciona todos os itens do carrosel e add o evento para alterar o slide principal
+function seletorClasses() {
+  const boxesLivros = document.querySelectorAll(".owl-item .item");
+
+  function handleClick({ target }) {
+    const id = target.dataset.boxlivro;
+    mainBookChange(id);
+  }
+
+  for (livro of boxesLivros) {
+    livro.addEventListener("click", handleClick);
+  }
+}
+
+// Menu mobile
+function menuMobile() {
+  const botaoMobile = document.querySelector("button[data-menuMobile]");
+
+  botaoMobile.addEventListener("click", handleClick);
+  botaoMobile.addEventListener("touchstart", handleClick);
+
+  function handleClick(event) {
+    event.preventDefault();
+    event.currentTarget.classList.toggle("ativo");
+  }
+}
+
+seletorClasses();
+menuMobile();
